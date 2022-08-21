a9d3e89792d7d36ea1ed5d38aa4e7ff2240a4233
Koh Uehara <refluster@gmail.com>
2022-08-21 01:27:30 -0700

    address form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+type Wallet = {
+	balance: number
+};
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	public wallet: Wallet| undefined;
+	public address: string = '';
 
-  constructor() { }
+	constructor(
+		private api: ApiService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	async getWallet() {
+		this.wallet = await this.api.getWalletInfo(this.address);
+		console.log(this.wallet);
+	}
 }
